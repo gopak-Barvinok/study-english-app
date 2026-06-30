15a9ddcc8e5ff8b7082db24611ba9249014e1f75
gopak-Barwinok <polpot09090@proton.me>
2026-06-30 16:57:37 +0100

    added footer to prod

--- components/FooterWrapper.tsx
+++ components/FooterWrapper.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 
 export default function FooterWrapper() {
-  const { data: session } = useSession();
   const pathName = usePathname();
 
-  if (!session || pathName !== "/") return null;
+  if (pathName !== "/") return null;
 
   return (
     <footer className="py-6 px-8 bg-base-200 border-t border-base-300 text-center text-base-content/40 text-sm">
